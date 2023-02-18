cf5a5fd1ff9f6dda98be1e24b4245f2835e57acf
unknown <gajanan.wankhede100@gmail.com>
2023-02-19 00:42:14 +0530

    spring data jpa example added

---
+++ src/main/java/com/example/springbootdatajpaexample/SpringBootDataJpaExampleApplication.java
@@ -0,0 +1,10 @@
+package com.example.springbootdatajpaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public cSpringApplication.run(SpringBootDataJpaExampleApplication.class, args);
+	}
+
+}
